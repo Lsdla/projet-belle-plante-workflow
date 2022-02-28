f6121ecd9d0d10df859cd6816cd4c3129d96483d
extons <diablotik-2203@hotmail.com>
2022-02-28 17:18:23 +0100

    Try: Github Action dependecies install

--- karma.conf.js
+++ karma.conf.js
@@ -34,23 +34,10 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
-    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage',
-          '--ignore-certificate-errors',
-          '--window-size=1920,1080'
-        ]
-      }
-    },
     singleRun: false,
     restartOnFileChange: true
   });
