805de10e0a727cda297c11b70a0696ee4352f9b1
extons <diablotik-2203@hotmail.com>
2022-03-01 15:30:14 +0100

    Fix: Json Server deployement

--- server.js
+++ server.js
@@ -6,3 +6,19 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/la-belle-plante/index.html'));});
 app.listen(process.env.PORT || 8080);
+
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('dist/la-belle-plante/data.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
